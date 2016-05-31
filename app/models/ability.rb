725d7c857c6dc58263fe04191c83280704a7db73
Steve Witman <stevewitman@gmail.com>
2016-05-31 01:04:21 -0600

    finalize steps

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,17 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new
+    if user.has_role? :admin
+      can :manage, :all
+    elsif user.has_role? :author
+      can :create, Status # author can create status
+      can :update, Status # author can update status
+      # can :destroy, Status # #uncomment this line, author can destroy status
+      can :read, :all
+    else
+      can :read, :all
+    end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160530221658) do
+ActiveRecord::Schema.define(version: 20160531054737) do
 
   create_table "statuses", force: :cascade do |t|
     t.text     "post"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160530221658) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "roles_mask"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
